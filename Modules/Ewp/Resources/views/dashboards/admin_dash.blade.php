780c5c0f6dca1830da5476a8a24d270c99e70135
Azid <azidgenji1234@um.edu.my>
2023-04-04 11:34:44 +0800

    UPDATE DASHING

--- Modules/Ewp/Resources/views/dashboards/admin_dash.blade.php
+++ Modules/Ewp/Resources/views/dashboards/admin_dash.blade.php
@@ -226,298 +226,15 @@
               </div>  
           </div>
       </div>
-<style>
-  *{
-    
-  }
-  .small-box,.headerss{
-    background-color: #FFFFFF;
-    color: #001f3f;
-  }
-  .description-header{
-    color: #001f3f;
-  }
-  .fa-school,.fa-user-tie,.fa-chart-pie{
-    color: white;
-    -webkit-text-stroke: 2px #001f3f;
-  }
-  .headerss{
-    background-color: #001f3f;
-    color: white;
-  }
-  .contoh{
-    width: 20em; 
-    float: right; 
-    padding:0.5em;
-  }
-  #ok1{
-    border-block-end-style: solid;
-    border-block-end-color: #001f3f;
-    padding:1em;
-    
-  }
-  #ok2{
-    border-block-end-style: solid;
-    border-block-end-color: #001f3f;
-      padding: 2em;
-    }
-  #try{
-    width: 30%;
-  }
-  td{
-    padding:10em;
-  }
-  .small-box{
-  border-block-start-style: solid;
-  border-block-start-color: #001f3f;
-  transform: translateY(3px);
-  animation: wavyText 10s ease-in-out infinite;
-  
-}
-#focy{
-  animation: wavyText2 10s ease-in-out infinite;
-}
-#mo{
-  animation-delay: 1s;
-}
-
-#mo2 {
-  animation-delay: 2s;
-}
-
-#mo3 {
-  animation-delay: 3s;
-}
-
-
-@keyframes wavyText {
-  0%{
-    
-  }
-  20%{
-    background: #1D3456;
-    color: white;
-    
-  }
-  40%{
-    background: #1D3456;
-    color: white;
-    
-  }
-  60% {
-    background: #1D3456;
-    color: white;
-    
-  }
-  80% {
-    background: #1D3456;
-    color: white;
-    
-  }
-  100% {
-  }
-}
-
-@keyframes wavyText2 {
-  0%{
-    
-  }
-  20%{
-    color: white;
-  }
-  40%{
-    color: white;
-  }
-  60% {
-    color: white;
-  }
-  80% {
-    color: white;
-  }
-  100% {
-  }
-}
-
-  @media(max-width: 424px){
-    .contoh{
-      padding-left: 1em;
-    }
-    #ok{
-      width: 15em;
-      padding: 2em;
-      border-style: solid;
-      border-color: black;
-      font-size: 1em;
-    }
-    #okss{
-      width: 100%;
-      padding: 1em;
-      border-style: solid;
-      border-color: black
-    }
-    .card{
-      border-style: solid;
-      border-color: black
-    }
-  }
-  #johns{
-    background: #1D3456;
-    color: #FFFFFF; 
-    width: 100%;
-  }
-  .date{
-    text-shadow: 0 1px 0 #CCCCCC, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 
-    0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 
-    0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);
-    font-weight: bold
-}
-
-.time{
-  text-shadow: 0 1px 0 #CCCCCC, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 
-    0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 
-    0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);
-  font-weight: bold
-}
-</style>
-@php
-
-    if(app()->currentLocale() == 'ms-my')
-    {
-        $title = 'Dashboard UM';
-        $tsurvey = 'Jumlah Tinjauan';
-        $tuser = 'Jumlah Pengguna';
-        $tvisitor = 'Jumlah Intervensi Khusus';
-
-        $stff = 'Staf';
-        $stdnt = 'Pelajar';
-
-        $title2 = 'Bilangan Pelajar Mengikut Kaunselor';
-        $name = 'Nama';
-        $complete = 'Selesai';
-        $reference = 'Rujuk';
-        $incomplete = 'Belum Selesai';
-        $total = 'Jumlah Yang Ditugaskan';
-
-        $title3 = 'Maklumat Saringan';
-        $label = 'Institusi Pengajian';
-        $yaxistext = 'Jumlah Pelajar';
-    }
-    
-    elseif(app()->currentLocale() == 'en')
-    {
-        $title = 'UM Dashboard';
-        $tsurvey = 'Total Survey';
-        $tuser = 'Total User';
-        $tvisitor = 'Total Special Intervention ';
-
-        $stff = 'Staff';
-        $stdnt = 'Student';
-
-        $title2 = 'Number of Students by Counselor';
-        $name = 'Name';
-        $complete = 'Complete';
-        $reference = 'Reference';
-        $incomplete = 'Incomplete';
-        $total = 'Total Assigned';
-
-        $title3 = 'Screening Information';
-        $label = 'Institution';
-        $yaxistext = 'Total Student';
-    }
-
-@endphp
-  <div class="d-flex">
-      <div class="mr-auto p-2" style="text-shadow: 0 1px 0 #CCCCCC, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);" ><h1>{{$title}}</h1></div>
-      <div class="p-1">
-              <nav aria-label="breadcrumb">
-                  <ol class="breadcrumb">
-                      <li class="breadcrumb-item"><a href="{{ route('ewp.dashboards.index') }}">Home</a></li>
-                      <li class="breadcrumb-item active" aria-current="page">Admin Dashboard</li>
-                  </ol>
-              </nav>
-              <div class="datetime">
-                <div class="date">
-                  <span id="dayname">Day</span>,
-                  <span id="month">Month</span>
-                  <span id="daynum">00</span>,
-                  <span id="year">Year</span>
-                </div>
-                <div class="time">
-                  <span id="hour">00</span>:
-                  <span id="minutes">00</span>:
-                  <span id="seconds">00</span>
-                  <span id="period">AM</span>
-                </div>
-              </div>  
-          </div>
-      </div>
 @stop
 
 @section('content')
 
 <div class="container-fluid" style="">
-<div class="container-fluid" style="">
     <br>
-    <body onload="initClock()">
     <body onload="initClock()">
         <section class="ftco-section">
             {{-- Header/Title & Manual/Start Test Button Section --}}
-            <div class="card-header">
-    
-              <div class="card-tools shadow-lg rounded">
-                  <div class="input-group-append">
-                    <form action="{{ route('ewp.dashboards.admin_dash', ['year' => $selectedYear]) }}" method="GET">
-                      <select class="form-control" name="year" id="year" onchange="this.form.submit()">
-                        @for( $x=date('Y')-1; $x <= date('Y')+1 ; $x++)
-                          <option value="{{ $x }}" @if($x ==  $selectedYear)selected @endif >{{ $x }}</option>
-                         @endfor
-                      </select>
-                  </form>
-                  </div>
-                </div>
-              </div>
-              {{-- @if($selectedYear == date('Y')) --}}
-              <div class="col-md-12">
-                <div class="row">
-                    <div class="col-md-4 col-15">
-                      <!-- small card -->
-                      <div id="mo" class="small-box shadow p-3 mb-5  rounded">
-                        <div class="inner">
-                          <h1>
-                            {{ $overallsurvey[0]->overallcount }}
-                          </h1>
-                          <p class="mb-4">{{$tsurvey}}</p>
-                        </div>
-                        <div class="icon">
-                          <i class="fas fa-school"></i>
-                        </div>
-                        <div class="card-footer">
-                          <div class="row">
-                            <div class="col-sm-6 border-right">
-                              <div class="description-block">
-                                <h5 id="focy" class="description-header">
-                                  {{$staffsurvey}}
-                                </h5>
-                                <span class="description-text">{{$stff}}</span>
-                              </div>
-                              <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
-                            <div class="col-sm-6">
-                              <div class="description-block">
-                                <h5 id="focy" class="description-header">
-                                  {{$studentsurvey}}
-                                </h5>
-                                <span class="description-text">{{$stdnt}}</span>
-                              </div>
-                              <!-- /.description-block -->
-                            </div>
-                            <!-- /.col -->
-                          </div>
-                          <!-- /.row -->
-                        </div>
-                      </div>
-                    </div>
             <div class="card-header">
     
               <div class="card-tools shadow-lg rounded">
@@ -600,35 +317,7 @@
                                     <div class="description-block">
                                       <h5 id="focy" class="description-header">{{ $data['total_staff'] }}</h5>
                                       <span class="description-text">{{$stdnt}}</span>
-                          <div class="col-md-4 col-12">
-                            <!-- small card -->
-                            <div id="mo2" class="small-box shadow p-3 mb-5  rounded" style="">
-                              <div class="inner">
-                                <h1>{{$data['total_user']}}</h1>
-
-                                <p class="mb-4">{{$tuser}}</p>
-                              </div>
-                              <div class="icon">
-                                <i class="fas fa-user-tie"></i>
-                              </div>
-                              <div class="card-footer">
-                                <div class="row">
-                                  <div class="col-sm-6 border-right">
-                                    <div class="description-block">
-                                      <h5 id="focy" class="description-header">{{ $data['total_student'] }}</h5>
-                                      <span class="description-text">{{$stff}}</span>
                                     </div>
-                                    <!-- /.description-block -->
-                                  </div>
-                                  <!-- /.col -->
-                                  <div class="col-sm-6">
-                                    <div class="description-block">
-                                      <h5 id="focy" class="description-header">{{ $data['total_staff'] }}</h5>
-                                      <span class="description-text">{{$stdnt}}</span>
-                                    </div>
-                                    <!-- /.description-block -->
-                                  </div>
-                                  <!-- /.col -->
                                     <!-- /.description-block -->
                                   </div>
                                   <!-- /.col -->
@@ -671,7 +360,6 @@
                             </div>
                           </div>
                         </div>
-                      
                       
                     </div>
                 </div>
@@ -974,4 +662,3 @@ return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
 </div>
   
 @endsection
-
